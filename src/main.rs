a06757553203790ae7043c4e8559102293697aae
Christian Blades <cblades@pindrop.com>
2017-01-19 10:41:34 -0500

    filter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate rustc_serialize;
 extern crate hyper;
 use rustc_serialize::json;
-use hyper::{client, Url};
-use std::io::Read;
-use std::env;
+use std::{env,clone};
+use std::iter::Iterator;
 
 #[derive(RustcDecodable,Debug)]
 #[allow(non_snake_case)]
@@ -20,15 +19,35 @@ pub struct Arrival {
 
 fn main() {
     let apikey = env::var("MARTA_API_KEY").unwrap();
-
-    match get_arrivals(&apikey) {
-        Some(arrivals) => println!("{:?}", arrivals),
-        None => return,
+    let arrivals = get_arrivals(&apikey).unwrap();
+    // println!("{:?}", arrivals);
+    assert!(String::from("DORAVILLE STATION") == "DORAVILLE STATION");
+    
+    let doraville_s = arrivals.into_iter().filter(|arr: &Arrival| -> bool {
+        arr.STATION == "DORAVILLE STATION"
+    });
+    for arr in doraville_s {
+        println!("{:?}", arr);
     }
+    
+    // let midtown_n = arrivals.into_iter().filter(|arr: &Arrival| -> bool {
+    //     arr.STATION == "MIDTOWN STATION" && arr.DIRECTION == 'N'
+    // });
+    // for arr in midtown_n {
+    //     println!("{:?}", arr);
+    // }
+                                                
+    // match get_arrivals(&apikey) {
+    //     Some(arrivals) => println!("{:?}", arrivals),
+    //     None => return,
+    // }
 }
 
 #[allow(unused_must_use)]
 fn get_arrivals(apikey: &str) -> Option<Vec<Arrival>> {
+    use hyper::{client, Url};
+    use std::io::Read;
+
     let mut rta = Url::parse("http://developer.itsmarta.\
                               com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals")
         .unwrap();
@@ -38,8 +57,8 @@ fn get_arrivals(apikey: &str) -> Option<Vec<Arrival>> {
 
     let cli = client::Client::new();
     let mut res = match cli.get(rta).send() {
-        Ok(x) => x,
-        Err(_) => return None,
+        Ok(x) => x, 
+       Err(_) => return None,
     };
     res.read_to_string(&mut buf);
 
